419fcdd00fb2e218d2b2807da1019694ee87d9e3
romankiper <roman_kiper@mail.ru>
2023-03-24 19:05:53 +0300

    add permission_2

--- lms/authentication/permissions.py
+++ lms/authentication/permissions.py
@@ -6,6 +6,21 @@ class IsAdmin(BasePermission):
         return bool(request.user and request.user.role.name == 'administrator')
 
 
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.role.name == 'manager')
+
+
+class IsMentor(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.role.name == 'mentor')
+
+
+class IsStudent(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.role.name == 'student')
+
+
 class IsAdminOrManager(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.role.name in ('administrator', 'manager'))
