f7f1e9ac943962bca7f41f1ef9753ab2ae0bfe56
Maxim <maxxim.zhurkin99@gmail.com>
2023-03-25 22:17:31 +0300

    mistakes soluted in settings

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manager.apps.ManagerConfig',
+    'docs.apps.DocsConfig',
     'rest_framework',
     'drf_yasg',
 ]
